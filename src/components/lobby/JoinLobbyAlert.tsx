bae7d50f277bbd39b752dcc1b1aec56f0c9a7fc4
gavhoang <gavinhoang@gmail.com>
2025-04-24 23:01:13 +0100

    wip

---
+++ src/components/lobby/JoinLobbyAlert.tsx
@@ -0,0 +1,24 @@
+import { AlertCircle } from "lucide-react"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
+interface JoinLobbyAlertProps  {
+  msg: string;
+}
+
+export function JoinLobbyAlert(message: JoinLobbyAlertProps) {
+
+  return (
+    <Alert variant="destructive" className="w-full max-w-sm mx-auto mb-4">
+      <AlertCircle className="h-4 w-4" />
+      <AlertDescription>
+        {message.msg}
+      </AlertDescription>
+    </Alert>
+  )
+
+}
